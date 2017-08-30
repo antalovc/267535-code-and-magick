a88d5c41c443ccd62d4bd334a36b4636e92fa0ba
antalovc <antal.ovchinnikov@gmail.com>
2017-08-30 20:17:24 +0300

    5.15 Добавлена возможность двигать диалог настройки персонажа и перетаскивать предметы в инвентаре

--- js/setup.js
+++ js/setup.js
@@ -68,4 +68,63 @@
     fireballSetup.style.backgroundColor = color;
   });
 
+  var setupShop = setupWindow.querySelector('.setup-artifacts-shop');
+  var draggedElement = null;
+
+  setupShop.addEventListener('dragstart', function (evt) {
+    var target = evt.target;
+    if (target.tagName.toLowerCase() === 'img') {
+      draggedElement = target;
+      evt.dataTransfer.setData('text/plain', target.alt);
+      markDraggableCells(true);
+    }
+  });
+
+  setupShop.addEventListener('dragend', function () {
+    markDraggableCells(false);
+  });
+
+  var setupArtifacts = setupWindow.querySelector('.setup-artifacts');
+  var dragCells = null;
+
+  var markDraggableCells = function (mark) {
+    if (mark) {
+      dragCells = setupWindow.querySelectorAll('.setup-artifacts .setup-artifacts-cell:empty');
+    }
+    dragCells.forEach(function (cell) {
+      cell.style.borderColor = mark ? 'red' : '';
+    });
+  };
+
+  var isEmptyDragTarget = function (target) {
+    return target.tagName.toLowerCase() !== 'img' && target.innerHTML === '';
+  };
+
+  setupArtifacts.addEventListener('dragover', function (evt) {
+    if (isEmptyDragTarget(evt.target)) {
+      evt.preventDefault();
+    }
+  });
+
+  setupArtifacts.addEventListener('drop', function (evt) {
+    evt.target.style.backgroundColor = '';
+    markDraggableCells(false);
+    evt.target.appendChild(draggedElement.cloneNode());
+    evt.preventDefault();
+  });
+
+  setupArtifacts.addEventListener('dragenter', function (evt) {
+    if (isEmptyDragTarget(evt.target)) {
+      evt.target.style.backgroundColor = 'yellow';
+      evt.preventDefault();
+    }
+  });
+
+  setupArtifacts.addEventListener('dragleave', function (evt) {
+    if (isEmptyDragTarget(evt.target)) {
+      evt.target.style.backgroundColor = '';
+      evt.preventDefault();
+    }
+  });
+
 })();
